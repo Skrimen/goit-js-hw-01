057b61f90a046895e416cfbffff3254dcd639617
Skrimen <mokosin1991@gmail.com>
2023-11-05 01:37:09 +0200

    yes

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,12 @@
 // const makeTransaction = (quantity, pricePerDroid)=>
 // `You ordered ${quantity} droids worth ${quantity*pricePerDroid} credits!`
 
-
-
-function makeTransaction(quantity, pricePerDroid){
-return `You ordered ${quantity} droids worth ${pricePerDroid*quantity} credits!`
+function makeTransaction(quantity, pricePerDroid) {
+  return `You ordered ${quantity} droids worth ${
+    pricePerDroid * quantity
+  } credits!`;
 }
 
-console.log(makeTransaction(5,3000));
-console.log(makeTransaction(3,1000));
-console.log(makeTransaction(10,500));
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
